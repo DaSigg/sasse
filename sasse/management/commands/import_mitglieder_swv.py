f9ba1690dab6473a0daf71e42de0153899e18e45
Daniel Steinmann <daniel.steinmann@swisscom.com>
2026-05-01 20:20:05 +0200

    Import: Verbesserter SWV Import
    - Ignore die Pontonierfahrvereine, die im SWV erfasst wurden
    - Korrektes Character Encoding der JSON Datei

--- sasse/management/commands/import_mitglieder_swv.py
+++ sasse/management/commands/import_mitglieder_swv.py
@@ -6,6 +6,25 @@ from datetime import date
 from django.core.management.base import BaseCommand, CommandError
 from sasse.models import Mitglied, Sektion
 
+EXCLUDED = [
+    '2', # Pontoniere Aarburg
+    '3', # Pontoniere Aarwangen
+    '5', # Basler Pontoniere
+    '8', # Pontoniere Bremgarten
+    '9', # Pontoniere Brugg
+    '10', # Pontoniere Buchs
+    '12', # Pontoniere Dietikon
+    '24', # Pontoniere Olten
+    '25', # Pontoniere Ottenbach
+    '26', # Pontoniere Rheinfelden
+    '27', # Pontoniere Schaffhausen
+    '29', # Pontoniere Schönenwerd
+    '30', # Pontoniere Schwaderloch
+    '35', # Pontoniere Wallbach
+    '38', # Pontoniere Wynau
+    '39', # Pontoniere Zurzach
+]
+
 class Command(BaseCommand):
     help = 'Importiert SWV Mitglieder Daten aus dem Json File.'
 
@@ -14,11 +33,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if 'JSON-FILE' not in options:
-            raise CommandError('Ein Excel File mit Mitgliederdaten als Input erwartet.')
+            raise CommandError('Ein JSON File mit Mitgliederdaten als Input erwartet.')
         path = options['JSON-FILE']
         if not os.path.isfile(path):
             raise CommandError('%s: Ist kein File.' % path)
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
             club_cache = self.import_sektionen(data['clubs'])
             self.import_mitglieder(data['participants'], club_cache)
@@ -29,11 +48,10 @@ class Command(BaseCommand):
         for i, club in enumerate(clubs):
             verein_name = club['name']
             verein_id = club['FK_VereinID']
-            if verein_id:
+            if verein_id and not verein_id in EXCLUDED:
                 try:
-                    s = Sektion.objects.get(nummer=verein_id)
-                    s.name = verein_name
-                    s.save()
+                    s = Sektion.objects.get(name=verein_name)
+                    self.stdout.write(self.style.SUCCESS("Existing: %s" % s))
                 except Sektion.DoesNotExist:
                     s = Sektion()
                     s.nummer = verein_id
@@ -41,6 +59,9 @@ class Command(BaseCommand):
                     s.save()
                     self.stdout.write(self.style.SUCCESS("Inserted: %s" % s))
                 cache[club['id']] = s
+            else:
+                self.stdout.write(self.style.SUCCESS("Ignoring: %s: %s" % (
+                    verein_id, verein_name)))
         self.stdout.write(self.style.SUCCESS("Import Vereine: %d" % i))
         return cache
 
@@ -53,13 +74,13 @@ class Command(BaseCommand):
     def import_mitglieder(self, participants, club_cache):
         for i, participant in enumerate(participants):
             fahrer_id = participant['FK_fahrerID']
-            if fahrer_id:
+            sektion = club_cache.get(participant['club_id'])
+            if fahrer_id and sektion:
                 nummer = "SWV-%d" % fahrer_id
                 name = participant['last_name']
                 vorname = participant['first_name']
                 geburtsdatum = date(participant['year_of_birth'], 1, 1)
                 geschlecht = self.map_geschlecht(participant['gender'])
-                sektion = club_cache[participant['club_id']]
                 try:
                     m = Mitglied.objects.get(nummer=nummer)
                     m.name = name
